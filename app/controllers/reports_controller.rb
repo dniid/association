50af9aa42d1d697de2f59fd047db31e28b36b142
dniid <dniidtf2@gmail.com>
2024-05-10 21:19:46 -0300

    Ajusta conflito com turbo

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def balance
     PersonMailer.balance_report(current_user).deliver_now
-    redirect_to root_path, notice: 'Relatório enviado para seu e-mail', method: :get
+    redirect_to root_path, notice: 'Relatório enviado para seu e-mail'
   end
 end
